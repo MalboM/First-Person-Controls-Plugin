687bd4dc549da02a917a124f9a14d6620cde0238
Fabio Ferrara <fabioferrara1989@gmail.com>
2015-10-20 18:35:51 +0200

    Assets2

---
+++ Assets/ScriptFabio/ChangeTexture.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeTexture : MonoBehaviour {
+
+
+	public Texture2D cavodrittoattivo;
+	public Texture2D cavodrittodisattivo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable(){
+		GetComponent<Renderer>().material.mainTexture = cavodrittoattivo;
+
+	}
+
+	void OnDisable(){
+		GetComponent<Renderer>().material.mainTexture = cavodrittodisattivo;
+	}
+}
